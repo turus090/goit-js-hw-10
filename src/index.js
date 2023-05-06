813d44b266481382f3459ac2e927870ca2cfd567
Sergii Tatianchenko <taurus090@gmail.com>
2023-05-06 16:29:33 +0300

    add country List

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
 import debounce from 'lodash.debounce';
 import fetchCountries from './fetchCountries';
 import './css/styles.css';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
 let listCountries
+const countriesList =  document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
 const DEBOUNCE_DELAY = 300;
 
 const renderListCountries = (list) => {
+    countryInfo.innerHTML = ''
+    countriesList.innerHTML = ''
+    list.forEach(country => {
+        countriesList.innerHTML += `
+        <li class="country-item">
+            <img class="country-flag" src="${country.flags.svg}" alt="${country.flags.alt}">
+            <p class="country-name">${country.name.official}</p>
+        </li>
+        `
+        
+    });
     console.log('it is list')
     console.table(list)
 }
 const renderOneCountry = (country) => {
+    countriesList.innerHTML = ''
+
     console.log('it is one country')
     console.log(country)
 }
 const renderToMuch = () =>{
+    Notify.info("Too many matches found. Please enter a more specific name.")
     console.log('it is to many countries')
 }
 const renderNotFound = () => {
     console.log('it is not found')
+    Notify.info("Oops, there is no country with that name")
 }
 
 const searchBox = document.querySelector('#search-box');
-const countryInfo = document.querySelector('.country-info');
-const countryList = document.querySelector('.country-list');
 searchBox.addEventListener('input', debounce(() =>{
     fetchCountries(searchBox.value.trim())
     .then(res => {
